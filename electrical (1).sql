62c5dbc68809b8c70e9ab7423c3483b46e1029bc
Олег Иваненко <ivanenko.oleg.m@gmail.com>
2021-02-10 18:55:38 +0200

    basic version completed

--- electrical (1).sql
+++ electrical (1).sql
@@ -5,7 +5,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-USE `electrical`;
+USE `ch95d83f8b`;
 
 DROP TABLE IF EXISTS `requests`;
 CREATE TABLE `requests` (
